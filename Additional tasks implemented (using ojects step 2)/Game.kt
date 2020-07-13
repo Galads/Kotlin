7087b0f1aeb4b00d9bead03db1e569ab257d5ac8
Galads <nikola_2212@mail.ru>
2020-07-14 00:05:51 +0300

    additional tasks to paragraph 7

---
+++ Additional tasks implemented (using ojects step 2)/Game.kt
@@ -0,0 +1,111 @@
+package com.bignerdranch.hucker
+
+import java.lang.Exception
+import java.lang.IllegalStateException
+
+fun main(args: Array<String>) {
+
+    Game.play()
+
+}
+
+object Game {
+    private var player = Player("galads", true, false)
+    private var currentRoom: Room = townSquare()
+
+    private var worldMap = listOf(
+        listOf(currentRoom, Room("Таверна"), Room("Задняя комната")),
+        listOf( Room("Длинный коридор"), Room("Общая комната"))
+    )
+
+
+     fun move(directionInput: String) =
+        try {
+            val direction = Direction.valueOf(directionInput.toUpperCase())
+            val newPosition = direction.updateCoordinate(player.currentPosition)
+
+            if (!newPosition.isInBounds){
+                throw IllegalStateException("$direction выходит за пределы")
+            }
+            val newRoom = worldMap[newPosition.y][newPosition.x]
+
+            player.currentPosition = newPosition
+            currentRoom = newRoom
+           // print(worldMap.flatten())
+
+
+            "Хорошо, вы двигаетесь на: $direction к локации ${newRoom.name}.\n ${newRoom.load()}"
+
+        }catch (e: Exception){
+            "Неверное направление: $directionInput."
+        }
+
+    private class GameInput(arg: String?){
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, {""})
+
+        fun processCommand() = when (command.toLowerCase()){
+            "map" -> printPosition()
+            "move" -> move(argument)
+            else -> commandNotFound()
+
+        }
+
+        private fun commandNotFound() = "Я не совсем понимаю что вы пытаетесь сделать"
+
+        }
+         fun printPosition(){
+             var position = player.currentPosition
+
+
+             println(position)
+             when(position){
+
+             }
+
+             val map:  Array<Array<String>> = Array(2, { Array(3, {"0"}) })
+             map[1][2] = ""
+             map[player.currentPosition.y][player.currentPosition.x] = "X"
+
+             for(row in map){
+                 for(cell in row){
+                     print("$cell\t")
+                 }
+                 println()
+             }
+        }
+
+    init {
+        println("Welcome adventurer")
+        player.castFireball()
+    }
+
+    fun play(){
+        while (true){
+            //game process
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            printPlayerStatus(player)
+
+            print("> Введите комманду: ")
+            //
+            var commandWhile = readLine()
+            if (commandWhile == "quit") {
+                print("Выход из игры")
+                break
+            }
+            //readLine() in println later
+            println(GameInput(commandWhile).processCommand())
+
+        }
+    }
+
+    private fun printPlayerStatus(player: Player){
+        println("(Аура: ${player.auraColor()}) " +
+                "(Благословение: ${if (player.isBlassed) "ДА" else "НЕТ"})")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
+}
+
---
+++ Additional tasks implemented (using ojects step 2)/README.md
@@ -0,0 +1,3 @@
+# Kotlin
+Released a simple exit from the program using the team and ASKII map player.
+![alt tag](https://github.com/Galads/picture/blob/master/rkotlin%20map.png)
